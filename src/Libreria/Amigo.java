d276319a01176bb85a9a95bc2b98e85e7d604bd8
daviimt <dmateomerino@gmail.com>
2021-04-28 09:46:47 +0200

    Ejemplo

---
+++ src/Libreria/Amigo.java
@@ -0,0 +1,19 @@
+package Libreria;
+
+public class Amigo {
+
+	public Amigo(String codigo, String nombreAmigo, String telefono, int cantLib) {
+		super();
+		this.codigo = codigo;
+		this.nombreAmigo = nombreAmigo;
+		this.telefono = telefono;
+		this.cantLib = cantLib;
+	}
+	String codigo;
+	String nombreAmigo;
+	String telefono;
+	int cantLib;
+	
+	
+	
+}
